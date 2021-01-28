63eb3152c5ee03cf701b5f07ddb5d0b08a788527
Hauke Ingwersen <hauke@sourceboat.com>
2021-01-28 16:52:18 +0100

    cleanup frontend

--- frontend/src/common/icon/index.js
+++ frontend/src/common/icon/index.js
@@ -1,15 +1,6 @@
 import PropTypes from 'prop-types'
 
 import search from './search'
-import like from './like'
-import comment from './comment'
-import retweet from './retweet'
-import home from './home'
-import stream from './stream'
-import filter from './filter'
-import tag from './tag'
-import up from './up'
-import down from './down'
 import logout from './logout'
 import list from './list'
 import cloud from './cloud'
@@ -35,16 +26,7 @@ Icon.propTypes = {
 }
 
 Icon.TYPES = {
-  comment,
-  like,
-  retweet,
   search,
-  home,
-  stream,
-  filter,
-  tag,
-  up,
-  down,
   logout,
   list,
   cloud
